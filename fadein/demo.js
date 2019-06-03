db8ff523d3b8e67120d4111c71cb1871ba823ef4
Taiga Ito <taiga248@gmail.com>
2019-06-03 17:17:39 +0900

    update

--- fadein/demo.js
+++ fadein/demo.js
@@ -1,17 +1,10 @@
 (function(){
-
-  $('#slide_menu').css({
-    right: '-200px',
-  });
+  $('#slide_menu').css({ right: '-200px',});
   $('#slide_menu')
   .on('mouseover', function(){
-    $(this).stop(true).animate({
-      right: 0
-    }, 200, 'easeInOutCubic');
+    $(this).stop(true).animate({ right: 0 }, 200, 'easeInOutCubic');
   })
   .on('mouseout', function(){
-    $(this).stop(true).animate({
-      right: '-200px'
-    }, 200, 'easeInOutCubic');
+    $(this).stop(true).animate({ right: '-200px'}, 200, 'easeInOutCubic');
   });
 });
