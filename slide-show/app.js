9d811a26f4c6ffe945eb069013eb0f47afca5c5a
taiga248 <taiga248@gmail.com>
2020-03-08 18:03:15 +0900

    Vanilla化計画

--- slide-show/app.js
+++ slide-show/app.js
@@ -1,13 +1,25 @@
 $(function(){
     let page=0;
-    let lastPage =parseInt($("#slide img").length-1);
-     $("#slide img").css("display","none");
-     $("#slide img").eq(page).css("display","block");
+    const slide = document.getElementById("slide")
+    //const slide__img = document.getElementsByClassName("slide__img")
+    let lastPage = slide.childElementCount -1
+
+    //slide__img.style.display = "none"
+    //slide__img[page].style.display = "block"
+    $("#slide img").css("display","none");
+    $("#slide img").eq(page).css("display","block");
     
     function changePage(){
-         $("#slide img").fadeOut(1300);
-         $("#slide img").eq(page).fadeIn(1300);
-    };
+        $("#slide img").fadeOut(1300);
+        $("#slide img").eq(page).fadeIn(1300);
+        /* slide__img.classList.add('fadeout').setTimeout(() => { 
+             slide__img.style.display = "none"; 
+        }, 1300);
+          
+        slide__img[page].classList.add("fading").setTimeout(() => {
+             slide__img.style.display = "block"; 
+        }, 1300); */
+    }
     
     let Timer;
     function startTimer(){
@@ -20,19 +32,20 @@ $(function(){
     function stopTimer(){
          clearInterval(Timer);
     }
-    
-    $("#right").click(function() {
-          stopTimer();
-          startTimer();
-          page === lastPage ? page=0 : page++;
-          changePage();
-     });
-    $("#left").click(function() {
-          stopTimer();
-          startTimer();
-          page === 0 ? page=lastPage : page--;
-          changePage();
-    });
 
+    const left = document.getElementById("left")
+    const right = document.getElementById("right")
+    right.addEventListener("click", () => {
+         stopTimer();
+         startTimer();
+         page === lastPage ? page=0 : page++;
+         changePage();
+    })
+    left.addEventListener("click", () => {
+         stopTimer();
+         startTimer();
+         page === 0 ? page=lastPage : page--;
+         changePage();
+    })
     startTimer();
 });
