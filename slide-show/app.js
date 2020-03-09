626b008ffde24539199801ed46dbd1fa22ab0969
taiga248 <taiga248@gmail.com>
2020-03-09 18:36:14 +0900

    Vanilla化

--- slide-show/app.js
+++ slide-show/app.js
@@ -1,51 +1,46 @@
-$(function(){
-    let page=0;
+document.addEventListener('DOMContentLoaded', () => {
     const slide = document.getElementById("slide")
-    //const slide__img = document.getElementsByClassName("slide__img")
-    let lastPage = slide.childElementCount -1
+    const slide__img = document.getElementsByClassName("slide__img")
+    const left = document.getElementById("left")
+    const right = document.getElementById("right")
+    const changeTime = 4000
+    let lastPage = slide.childElementCount - 1
+    let page = 0
+    let Timer
 
-    //slide__img.style.display = "none"
-    //slide__img[page].style.display = "block"
-    $("#slide img").css("display","none");
-    $("#slide img").eq(page).css("display","block");
-    
     function changePage(){
-        $("#slide img").fadeOut(1300);
-        $("#slide img").eq(page).fadeIn(1300);
-        /* slide__img.classList.add('fadeout').setTimeout(() => { 
-             slide__img.style.display = "none"; 
-        }, 1300);
-          
-        slide__img[page].classList.add("fading").setTimeout(() => {
-             slide__img.style.display = "block"; 
-        }, 1300); */
-    }
-    
-    let Timer;
+         for(let i=0; i<slide__img.length; i++){
+              if(page === i){
+                   slide__img[page].style.display = "block"
+               }else{
+                    slide__img[i].style.display = "none"
+               }
+          }
+     }
+
     function startTimer(){
-          Timer =setInterval(function(){
-               page === lastPage ? page=0 : page++;
-               changePage();
-          },4000);
+          Timer =setInterval(() => {
+               page === lastPage ? page=0 : page++
+               changePage()
+          }, changeTime)
     }
 
     function stopTimer(){
-         clearInterval(Timer);
+         clearInterval(Timer)
     }
 
-    const left = document.getElementById("left")
-    const right = document.getElementById("right")
     right.addEventListener("click", () => {
-         stopTimer();
-         startTimer();
-         page === lastPage ? page=0 : page++;
-         changePage();
+         stopTimer()
+         startTimer()
+         page === lastPage ? page=0 : page++
+         changePage()
     })
     left.addEventListener("click", () => {
-         stopTimer();
-         startTimer();
-         page === 0 ? page=lastPage : page--;
-         changePage();
+         stopTimer()
+         startTimer()
+         page === 0 ? page = lastPage : page--
+         changePage()
     })
-    startTimer();
-});
+
+    startTimer()
+})
