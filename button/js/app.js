27b6c646161d612428736ac1209e3810c431f759
t.ito <t.ito@sonicmoov.com>
2021-05-29 02:52:37 +0900

    Update

--- button/js/app.js
+++ button/js/app.js
@@ -1,8 +1,15 @@
-const btn = document.getElementById("btn");
+const btn_1 = document.querySelector(".js-btn--1");
+const btn_2 = document.querySelector(".js-btn--2");
 
-btn.onmousemove = function (e) {
-  const x = e.pageX - btn.offsetLeft;
-  const y = e.pageY - btn.offsetTop;
-  btn.style.setProperty("--x" + x + "px");
-  btn.style.setProperty("--y" + y + "px");
+btn_1.onmousemove = function (e) {
+  const x = e.pageX - btn_1.offsetLeft;
+  const y = e.pageY - btn_1.offsetTop;
+  btn_1.style.setProperty("--x", x + "px");
+  btn_1.style.setProperty("--y", y + "px");
+};
+btn_2.onmousemove = function (e) {
+  const x = e.pageX - btn_2.offsetLeft;
+  const y = e.pageY - btn_2.offsetTop;
+  btn_2.style.setProperty("--x", x + "px");
+  btn_2.style.setProperty("--y", y + "px");
 };
